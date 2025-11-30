ce9d7c33f804ac8dc03fd2df83b5789578eeda77
Brian Kaether <bkaether@stanford.edu>
2025-11-30 00:22:15 -0500

    Normalize line endings

--- rtl/testbenches/generate_ray_tb.sv
+++ rtl/testbenches/generate_ray_tb.sv
@@ -2,14 +2,16 @@
 
 `include "../data_macros.sv"
 
+import tb_pkg::*;
+
 module generate_ray_tb();
 
     // testbench knobs
-    localparam NUM_TEST_PIXELS = 50;
-    localparam PIXEL_WIDTH = 800;
-    localparam PIXEL_HEIGHT = 600;
-    localparam FOV_DEGREES = 70;
-    localparam ERROR_THRESHOLD = 0.01;
+    localparam int NUM_TEST_PIXELS = 50;
+    localparam int PIXEL_WIDTH = 800;
+    localparam int PIXEL_HEIGHT = 600;
+    localparam real FOV_DEGREES = 70.0;
+    localparam real ERROR_THRESHOLD = 0.01;
 
     // error counter
     int num_errors = 0;
@@ -18,17 +20,17 @@ module generate_ray_tb();
     int test_pixels_x[NUM_TEST_PIXELS];
     int test_pixels_y[NUM_TEST_PIXELS];
 
-    // result arrays
+    // expected output arrays
     real expected_dir_x[NUM_TEST_PIXELS];
     real expected_dir_y[NUM_TEST_PIXELS];
-    real expected_dir_z[NUM_TEST_PIXELS];
 
+    // actual DUT output array
     ray actual_rays[NUM_TEST_PIXELS];
 
-    // random values used by the hardware for each calculation in Q0.16 fixed point format, for use in calculating expected output
+    // random values used by the hardware for each calculation in Q0.16 fixed point format
     bit [15:0] prng_x_fxpt[NUM_TEST_PIXELS];
     bit [15:0] prng_y_fxpt[NUM_TEST_PIXELS];
-    // real decimal random values used by hardware
+    // hardware generated random values converted to real decimal format for use in expected output calculation
     real prng_x[NUM_TEST_PIXELS];
     real prng_y[NUM_TEST_PIXELS];
 
@@ -43,8 +45,6 @@ module generate_ray_tb();
     // outputs
     ray generated_ray;
 
-    integer i;
-
     always #4 clk = ~clk;
 
     generate_ray DUT (
@@ -57,15 +57,18 @@ module generate_ray_tb();
         .generated_ray(generated_ray)
     );
 
-    task test_dut();
-        for (i = 0; i < NUM_TEST_PIXELS; i = i + 1) begin
+    task automatic test_dut();
+        for (integer i = 0; i < NUM_TEST_PIXELS; i = i + 1) begin
             // send pixel coords to DUT
             pixel_x = test_pixels_x[i];
             pixel_y = test_pixels_y[i];
 
-            // capture prng values used
+            // capture prng values used for multiplication
             prng_x_fxpt[i] = DUT.offset_x;
             prng_y_fxpt[i] = DUT.offset_y;
+
+            // wait for multiply IP to sample inputs
+            @(posedge clk);
 
             // wait 4 cycle latency before storing DUT output
             repeat (4) @(posedge clk);
@@ -75,25 +78,24 @@ module generate_ray_tb();
         end
     endtask
 
-    task compute_expected();
+    task automatic compute_expected();
         // calculate sensor plane width and height
         real vert_fov_rad;
         real sensor_plane_height;
         real sensor_plane_width;
 
-        vert_fov_rad = (FOV_DEGREES * $pi) / 180;
+        vert_fov_rad = FOV_DEGREES * DEG2RAD;
         sensor_plane_height = 2 * $atan(vert_fov_rad);
-        sensor_plane_width = (PIXEL_WIDTH / PIXEL_HEIGHT) * sensor_plane_height;
+        sensor_plane_width = (real'(PIXEL_WIDTH) / real'(PIXEL_HEIGHT)) * sensor_plane_height;
 
-        for (i = 0; i < NUM_TEST_PIXELS; i = i + 1) begin
+        for (integer i = 0; i < NUM_TEST_PIXELS; i = i + 1) begin
             // convert fxpt offests from prng to real decimal offsets
-            prng_x[i] = prng_x_fxpt[i] / 65536; // 16 fractional bits
-            prng_y[i] = prng_y_fxpt[i] / 65536;
+            prng_x[i] = real'(prng_x_fxpt[i]) / 65536.0; // 16 fractional bits
+            prng_y[i] = real'(prng_y_fxpt[i]) / 65536.0;
             
             // calculate camera space x and y
             expected_dir_x[i] = ((test_pixels_x[i] + prng_x[i]) / PIXEL_WIDTH * sensor_plane_width) - (sensor_plane_width / 2);
             expected_dir_y[i] = ((test_pixels_y[i] + prng_y[i]) / PIXEL_HEIGHT * sensor_plane_height) - (sensor_plane_height / 2);
-            expected_dir_z[i] = -1;
 
         end
 
@@ -101,9 +103,9 @@ module generate_ray_tb();
 
     initial begin
         // generate pixel values for test stimulus
-        for (i = 0; i < NUM_TEST_PIXELS; i = i + 1) begin
-            test_pixels_x[i] = $urandomrange(0, 800);
-            test_pixels_y[i] = $urandomrange(0, 600);
+        for (integer i = 0; i < NUM_TEST_PIXELS; i = i + 1) begin
+            test_pixels_x[i] = $urandom_range(PIXEL_WIDTH-1, 0);
+            test_pixels_y[i] = $urandom_range(PIXEL_HEIGHT-1, 0);
         end
 
         // reset sequence
@@ -129,19 +131,34 @@ module generate_ray_tb();
         compute_expected();
 
         // check_output
-        for (i = 0; i < NUM_TEST_PIXELS; i = i + 1) begin
-            if ($abs(actual_rays[i].dir.x - expected_dir_x[i]) > ERROR_THRESHOLD) begin
+        for (integer i = 0; i < NUM_TEST_PIXELS; i = i + 1) begin
+            automatic real actual_x = real'(actual_rays[i].dir.x) / 65536.0;
+            automatic real actual_y = real'(actual_rays[i].dir.y) / 65536.0;
+
+            automatic real diff_x = actual_x - expected_dir_x[i];
+            automatic real diff_y = actual_y - expected_dir_y[i];
+
+            if (abs_real(diff_x) > ERROR_THRESHOLD) begin
                 num_errors = num_errors + 1;
+                $error("X direction mismatch. Expected %f, got %f", expected_dir_x[i], actual_x);
+            end else begin
+                $display("Matched X direction for test pixel %d", i);
             end
 
-            if ($abs(actual_rays[i].dir.y - expected_dir_y[i]) > ERROR_THRESHOLD) begin
+            if (abs_real(diff_y) > ERROR_THRESHOLD) begin
                 num_errors = num_errors + 1;
+                 $error("Y direction mismatch. Expected %f, got %f", expected_dir_y[i], actual_y);
+            end else begin
+                $display("Matched Y direction for test pixel %d", i);
             end
         end
 
-        assert(num_errors == 0)
+        assert(num_errors === 0) else begin
+            $fatal("Actual output did not match expected output. Number of errors: %0d", num_errors);
+            $finish();
+        end
 
-        $display("Test Finsihed");
+        $display("Test Passed!");
         $finish();
     end
 
